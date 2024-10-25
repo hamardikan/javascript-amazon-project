9324cf7c74c55389f5d65619385f3781d3a18329
adhmrdkn <a.hamardikan@gmail.com>
2024-10-25 17:27:04 +0700

    add interactive cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary 
-          js-add-to-cart" data-product-name="${product.name}">
+          js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div> 
@@ -61,11 +61,11 @@ innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").
 forEach((button) => {
     button.addEventListener('click', () => {
-        const productName = button.dataset.productName;
+        const productId = button.dataset.productId;
         let matchingItem;
-
+ 
         cart.forEach((item) => {
-            if(productName ===  item.productName){
+            if(productId ===  item.productName){
                 matchingItem = item;
             }
         });
@@ -74,11 +74,18 @@ forEach((button) => {
             matchingItem.quantity += 1;
         } else {
             cart.push({
-            productName: productName,
+            productId: productId,
             quantity: 1
         });
         };
 
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        })
+        
+        document.querySelector(".js-cart-quantity").
+        innerHTML = cartQuantity;
 
     })
 })
